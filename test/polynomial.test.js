984f50da29cd23ed90937fa0f574414133f08dd5
Boris Chumichev <boris362@yandex.ru>
2014-10-12 21:45:30 +0400

    Polynomial interpolation implementation

--- test/polynomial.test.js
+++ test/polynomial.test.js
@@ -1,16 +1,16 @@
 'use strict'
 
 var assert = require("chai").assert
-  , polynomial = require("../lib/polynomial.js")
+  , polynomial = require("../lib/everpolate.js").polynomial
 
 describe('Polynomial interpolation function', function() {
 
   it('Evaluates interpolating polynomial at the number \'x\'', function(){
-    assert.equal( polynomial(3, [0, 1, 2], [1, 3, 2]), -2)
+    assert.deepEqual( polynomial(3, [0, 1, 2], [1, 3, 2]), [-2])
   })
 
   it('Evaluates interpolating polynomial at the set of numbers \'x1, x2,..., xn\'', function(){
-    assert.equal( polynomial([3, 0], [0, 1, 2], [1, 3, 2]), [-2, 1])
+    assert.deepEqual( polynomial([3, 0], [0, 1, 2], [1, 3, 2]), [-2, 1])
   })
 
 })
