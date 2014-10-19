c54b2dc1786c705073897ac5a65a75a9b914d173
Boris Chumichev <boris362@yandex.ru>
2014-10-19 18:03:56 +0400

    Linear Regression implemented

---
+++ test/linearRegression.test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var assert = require("chai").assert
+  , linearRegression = require("../lib/everpolate.js").linearRegression
+
+describe('Linear regression function', function () {
+
+  var xValues = [0, 1, 2, 3, 4, 5]
+    , yValues = [2, 3, 4, 5, 6, 7]
+    , regression = linearRegression(xValues, yValues)
+
+  it('Returns an object with slope, intercept and rSquared properties', function(){
+    assert.strictEqual(regression.slope, 1)
+    assert.strictEqual(regression.intercept, 2)
+    assert.strictEqual(regression.rSquared, 1)
+  })
+
+  it('Returns a function which computes regression at a given x-value', function () {
+    assert.strictEqual(regression.evaluate(2), 4)
+  })
+
+})
