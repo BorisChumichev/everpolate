9de2092c86f63ec668e349a9d3e87eb174018d89
Boris Chumichev <boris362@yandex.ru>
2014-10-22 02:05:08 +0400

    gh-pages initial

---
+++ gh-pages/js/mathjax-config.js
@@ -0,0 +1,9 @@
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
+  showMathMenu: false,
+  showMathMenuMSIE: false,
+  extensions: ["tex2jax.js"],
+  "HTML-CSS": {
+    scale: 80
+  }
+});
