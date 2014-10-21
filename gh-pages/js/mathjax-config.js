0ae82f4c9391a4ba2deb68966507104abb0ff935
Boris Chumichev <boris362@yandex.ru>
2014-10-22 02:16:06 +0400

    Get rid of project files

--- gh-pages/js/mathjax-config.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
-  showMathMenu: false,
-  showMathMenuMSIE: false,
-  extensions: ["tex2jax.js"],
-  "HTML-CSS": {
-    scale: 80
-  }
-});
